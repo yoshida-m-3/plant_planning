45ccf4b5a4ff612c8dd9b4d49da2ec0021ba9272
makoto <milmed1013@gmail.com>
2021-06-09 23:32:37 +0900

    カードサンプルの追加

--- lib/pages/PlantScreen.dart
+++ lib/pages/PlantScreen.dart
@@ -4,13 +4,29 @@ import 'package:flutter/material.dart';
 class PlantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Plant',
-            style: TextStyle(fontSize: 16),
+    return Center(
+        child: Card(
+            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      const ListTile(
+        leading: Icon(Icons.album),
+        title: Text('The Enhanted Nightingale'),
+        subtitle: Text('Music by Julie Gable Lyrics by Sidney Stein.'),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          TextButton(
+            child: const Text('BUY TICKETS'),
+            onPressed: () {},
           ),
-        ),
-        body: Center(child: Icon(Icons.book)));
+          const SizedBox(width: 8),
+          TextButton(
+            child: const Text('LISTEN'),
+            onPressed: () {},
+          ),
+          const SizedBox(width: 8),
+        ],
+      )
+    ])));
   }
 }
