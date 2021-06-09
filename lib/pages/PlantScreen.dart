92d2db55fb492a3e604102d14bd3cf5d9cda2442
makoto <milmed1013@gmail.com>
2021-06-09 23:58:29 +0900

    Grid追加

--- lib/pages/PlantScreen.dart
+++ lib/pages/PlantScreen.dart
@@ -4,29 +4,59 @@ import 'package:flutter/material.dart';
 class PlantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Card(
-            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-      const ListTile(
-        leading: Icon(Icons.album),
-        title: Text('The Enhanted Nightingale'),
-        subtitle: Text('Music by Julie Gable Lyrics by Sidney Stein.'),
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
+    return GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
         children: <Widget>[
-          TextButton(
-            child: const Text('BUY TICKETS'),
-            onPressed: () {},
-          ),
-          const SizedBox(width: 8),
-          TextButton(
-            child: const Text('LISTEN'),
-            onPressed: () {},
-          ),
-          const SizedBox(width: 8),
-        ],
-      )
-    ])));
+          Card(
+              child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enhanted Nightingale'),
+              subtitle: Text('Music by Julie Gable Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+              ],
+            )
+          ])),
+          Card(
+              child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enhanted Nightingale'),
+              subtitle: Text('Music by Julie Gable Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+              ],
+            )
+          ]))
+        ]);
   }
 }
